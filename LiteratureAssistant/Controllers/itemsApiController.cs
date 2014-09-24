d3c9aab0491da802b4d794602f96f4491db3b2e8
txavier <txavier@gmail.com>
2014-09-23 23:05:28 -0400

    - Added async webapi controller.

---
+++ LiteratureAssistant/Controllers/itemsApiController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LiteratureAssistant.Core.Models;
+using LiteratureAssistant.Data;
+
+namespace LiteratureAssistant.Controllers
+{
+    public class itemsApiController : ApiController
+    {
+        private LiteratureAssistantDbModel db = new LiteratureAssistantDbModel();
+
+        // GET: api/itemsApi
+        public IQueryable<item> Getitems()
+        {
+            return db.items;
+        }
+
+        // GET: api/itemsApi/5
+        [ResponseType(typeof(item))]
+        public async Task<IHttpActionResult> Getitem(int id)
+        {
+            item item = await db.items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // PUT: api/itemsApi/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putitem(int id, item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != item.itemId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!itemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/itemsApi
+        [ResponseType(typeof(item))]
+        public async Task<IHttpActionResult> Postitem(item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.items.Add(item);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = item.itemId }, item);
+        }
+
+        // DELETE: api/itemsApi/5
+        [ResponseType(typeof(item))]
+        public async Task<IHttpActionResult> Deleteitem(int id)
+        {
+            item item = await db.items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            db.items.Remove(item);
+            await db.SaveChangesAsync();
+
+            return Ok(item);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool itemExists(int id)
+        {
+            return db.items.Count(e => e.itemId == id) > 0;
+        }
+    }
+}
--- LiteratureAssistant/Global.asax.cs
+++ LiteratureAssistant/Global.asax.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
+using System.Web.Http;
 using System.Web.Routing;
+
 
 namespace LiteratureAssistant
 {
@@ -12,6 +14,7 @@ namespace LiteratureAssistant
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
